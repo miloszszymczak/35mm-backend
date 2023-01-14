9d9c2c3bbc50a4583f68880bd5318e62375fa32f
miloszszymczak <szymczak.milosz@gmail.com>
2023-01-14 12:49:27 +0100

    Initial commit

---
+++ src/main/java/pl/miloszszymczak/reelstack/ReelstackApplication.java
@@ -0,0 +1,13 @@
+package pl.miloszszymczak.reelstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReelstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReelstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/miloszszymczak/reelstack/ReelstackApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.miloszszymczak.reelstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReelstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
